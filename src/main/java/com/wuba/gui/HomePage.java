195a208f040d99f804eec814e39f57a89d5a02c1
542113578@qq.com <542113578@qq.com>
2015-07-15 11:37:45 +0800

    update

--- src/main/java/com/wuba/gui/HomePage.java
+++ src/main/java/com/wuba/gui/HomePage.java
@@ -1,9 +1,9 @@
 package com.wuba.gui;
 
+
 public class HomePage {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
 	}
 
--- src/test/java/com/wuba/logparser/AndroidLogParserTest.java
+++ src/test/java/com/wuba/logparser/AndroidLogParserTest.java
@@ -1,20 +1,26 @@
 package com.wuba.logparser;
 
+import java.io.File;
+
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
 
 
 
 public class AndroidLogParserTest {
-//	@BeforeClass
-//	public void setUp() {
-//
-//	}
-//
-//	@Test(groups = { "init" })
-//	public void logInitTest() {
-//		LogParser androidLog = new AndroidLogParser();
-//		File file = new File("/Users/wuxian/Documents/sourcecode/self/ResponseTimeTool/log/android/time_points.txt");
-//		String timeStamp = androidLog.parserLog(file);
-//		
-////		Assert.assertNotNull(timeStamp);
-//	}
+	@BeforeClass
+	public void setUp() {
+
+	}
+
+	@Test(groups = { "init" })
+	public void logInitTest() {
+		LogParser androidLog = new AndroidLogParser();
+		File file = new File("/Users/wuxian/Documents/sourcecode/self/ResponseTimeTool/log/android/time_points.txt");
+		String timeStamp = androidLog.parserLog(file);
+		
+		Assert.assertNotNull(timeStamp);
+	}
 }
