3184b2fc177000174b3591ece782984068fe04e7
542113578@qq.com <542113578@qq.com>
2015-07-20 17:40:44 +0800

    update

--- src/test/java/com/wuba/result/TestResultTest.java
+++ src/test/java/com/wuba/result/TestResultTest.java
@@ -9,6 +9,10 @@ import org.apache.log4j.Logger;
 import org.testng.annotations.BeforeGroups;
 import org.testng.annotations.Test;
 
+import com.wuba.result.DeviceInfo;
+import com.wuba.result.TestCases;
+import com.wuba.result.TestResult;
+
 /**
  * @author hui.qian qianhui@58.com
  * @date 2015年7月20日 下午4:24:04
@@ -17,19 +21,34 @@ public class TestResultTest {
 	private static final Logger LOGGER = Logger.getLogger(TestResultTest.class);
 
 	private TestResult testResult = null;
+	private File file = null;
 
 	@BeforeGroups(groups = { "unittest" })
 	public void setUp() {
 		testResult = new TestResult();
+		file = new File("testResult.xml");
+
 	}
 
 	@Test(groups = { "unittest" })
 	public void generateXMLTest() {
-		testResult.serializeResultToXml(new File("testResult.xml"));
+		testResult.serializeResultToXml(file);
 	}
 
 	@Test(groups = { "unittest" })
 	public void generateDeviceInfoTest() {
+		DeviceInfo deviceInfo = testResult.getDeviceInfo();
+		deviceInfo.setDeviceName("N6");
+		deviceInfo.setNetwork("4G");
+		testResult.serializeResultToXml(file);
+	}
+	
+	public void generateTestCasesTest(){
+		TestCases testCases = testResult.getTestCases();
+		
 		
 	}
+	
+	
+	
 }
