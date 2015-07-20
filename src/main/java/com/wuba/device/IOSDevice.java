c05fe9c3c9ebf6ec562dbbbe78453f9959564f1a
vigossjjj <jiangyunpeng@58.com>
2015-07-20 16:38:32 +0800

    make sure that temp dir cleaned before record, add func that get lastest device screenshot.

--- src/main/java/com/wuba/device/IOSDevice.java
+++ src/main/java/com/wuba/device/IOSDevice.java
@@ -1,14 +1,19 @@
 package com.wuba.device;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
 import com.wuba.utils.Constant;
 import com.wuba.utils.Helper;
 
 /**
  * IOS设备信息，控制
+ * 
  * @author vigoss
  *
  */
-public class IOSDevice implements Device{
+public class IOSDevice implements Device {
 
 	private String platform;
 	private String deviceId;
@@ -22,15 +27,18 @@ public class IOSDevice implements Device{
 		// TODO Auto-generated constructor stub
 		this.deviceId = deviceId;
 		this.appId = appId;
+		// 清空temp目录
+		Helper.deleteDirectory(Constant.iOS_TEMP_DIR);
+		Helper.createDir(Constant.iOS_TEMP_DIR);
 	}
-	
+
 	public void setPlatform(String platform) {
 		this.platform = platform;
 	}
 
-//	public void setDeviceId(String deviceId) {
-//		this.deviceId = deviceId;
-//	}
+	// public void setDeviceId(String deviceId) {
+	// this.deviceId = deviceId;
+	// }
 
 	public void setDeviceName(String deviceName) {
 		this.deviceName = deviceName;
@@ -40,9 +48,9 @@ public class IOSDevice implements Device{
 		this.osVersion = osVersion;
 	}
 
-//	public void setAppId(String appId) {
-//		this.appId = appId;
-//	}
+	// public void setAppId(String appId) {
+	// this.appId = appId;
+	// }
 
 	public void setScreenSize(int[] screenSize) {
 		this.screenSize = screenSize;
@@ -101,53 +109,83 @@ public class IOSDevice implements Device{
 		// TODO Auto-generated method stub
 
 	}
-	
-	public String sendActionCommand(String command){
-		System.out.println("SEND_CMD:"+command);
-		Helper.createFileAndWrite(command, Constant.iOS_CMD_FILE);
-		String resp = Helper.readFileTimeOut(Constant.iOS_RESP_FILE);
-		System.out.println("RESPONSE:"+resp);
-		Helper.deleteFile(Constant.iOS_RESP_FILE);
-		if (resp == null){System.exit(0);};
-		return resp.trim();
-	}
-	
+
 	/**
-	 * 启动录制服务
+	 * 获取最新屏幕截图
+	 * 
 	 * @return
 	 */
-	public boolean connectRecordServer(){
-		//删除cmd.txt resp.txt
-		Helper.deleteFile(Constant.iOS_CMD_FILE);
+	public String getLastScreenShot() {
+		List<File> results = new ArrayList<File>();
+		File[] files = new File(Constant.iOS_RUN1_DIR).listFiles();
+		if (files == null) {
+			return null;
+		}
+		for (File file : files) {
+			if (file.isFile() && file.getName().startsWith("screen")) {
+				results.add(file);
+			}
+		}
+
+		if (results.size() == 0) {
+			return null;
+		}
+
+		for (int i = 0; i < results.size() - 2; i++) {
+			results.get(i).delete();
+		}
+		return results.get(results.size() - 1).getName();
+	}
+
+	/**
+	 * 向设备发送UIAutomation命令
+	 * @param command
+	 * @return 响应结果
+	 */
+	public String sendActionCommand(String command) {
+		System.out.println("SEND_CMD:" + command);
+		Helper.createFileAndWrite(command, Constant.iOS_CMD_FILE);
+		String resp = Helper.readFileTimeOut(Constant.iOS_RESP_FILE);
+		System.out.println("RESPONSE:" + resp);
 		Helper.deleteFile(Constant.iOS_RESP_FILE);
-		
+		if (resp == null) {
+			System.exit(0);
+		}
+		;
+		return resp.trim();
+	}
+
+	/**
+	 * 启动录制服务
+	 * 
+	 * @return
+	 */
+	public boolean connectRecordServer() {
 		Thread thread = new Thread(new Runnable() {
 			@Override
 			public void run() {
-				Helper.executeCommand("sh " + Constant.iOS_RECORD_SHELL + " " + 
-						deviceId + " " + 
-						appId + " " + 
-						Constant.iOS_LOCAL_DIR);
+				Helper.executeCommand("sh " + Constant.iOS_RECORD_SHELL + " " + deviceId + " " + appId + " " + Constant.iOS_LOCAL_DIR);
 			}
 		});
 		thread.start();
-		
+
 		String resp = sendActionCommand(Constant.iOS_START_SERVER_CMD);
-		if (resp.equals("0|")){
+		if (resp.equals("0|")) {
 			serverConnected = true;
 			return true;
 		}
 		return false;
 	}
-	
+
 	/**
 	 * 断开录制服务
+	 * 
 	 * @return
 	 */
-	public boolean disConnectRecordServer(){
-		if (serverConnected){
+	public boolean disConnectRecordServer() {
+		if (serverConnected) {
 			String resp = sendActionCommand(Constant.iOS_CLOSE_SERVER_CMD);
-			if (resp.equals("1|")){
+			if (resp.equals("1|")) {
 				serverConnected = false;
 				return true;
 			}
--- src/main/java/com/wuba/utils/Helper.java
+++ src/main/java/com/wuba/utils/Helper.java
@@ -22,12 +22,12 @@ public class Helper {
 		Process p;
 		try {
 			p = Runtime.getRuntime().exec(command);
-//			p.waitFor();
+			// p.waitFor();
 			BufferedReader reader = new BufferedReader(new InputStreamReader(p.getInputStream()));
 
 			String line;
 			while ((line = reader.readLine()) != null) {
-				System.out.println("INST: "+line);
+				System.out.println("INST: " + line);
 				output.append(line + "\n");
 			}
 
@@ -38,13 +38,14 @@ public class Helper {
 		return output.toString();
 
 	}
-	
+
 	/**
 	 * 创建文件，并写入内容，非追加，如何已存在文件会覆写
+	 * 
 	 * @param line
 	 * @param filename
 	 */
-	public static void createFileAndWrite(String line, String filename){
+	public static void createFileAndWrite(String line, String filename) {
 		FileWriter fw;
 		try {
 			fw = new FileWriter(filename, false);
@@ -56,9 +57,10 @@ public class Helper {
 			e.printStackTrace();
 		}
 	}
-	
+
 	/**
 	 * 读取文件
+	 * 
 	 * @param fileName
 	 */
 	public static String readFileByLines(String fileName) {
@@ -85,42 +87,100 @@ public class Helper {
 		}
 		return output.toString();
 	}
-	
+
 	/**
 	 * 读取文件，超时：10s
+	 * 
 	 * @param filename
 	 * @return
 	 */
-	public static String readFileTimeOut(String filename){
+	public static String readFileTimeOut(String filename) {
 		long startTime = System.currentTimeMillis();
-		long timeOut = 10*1000;
+		long timeOut = 10 * 1000;
 		File file = new File(filename);
 		boolean exist = file.exists();
-		while(!exist){
+		while (!exist) {
 			long durationTime = System.currentTimeMillis() - startTime;
 			if (durationTime > timeOut) {
 				return null;
 			}
 			exist = file.exists();
 		}
-		
+
 		return readFileByLines(filename);
 	}
-	
-	/** 
-	 * 删除单个文件 
-	 * @param   sPath    被删除文件的文件名 
-	 * @return 单个文件删除成功返回true，否则返回false 
-	 */  
-	public static boolean deleteFile(String sPath) {  
-	    boolean flag = false;  
-	    File file = new File(sPath);  
-	    // 路径为文件且不为空则进行删除  
-	    if (file.isFile() && file.exists()) {  
-	        file.delete();  
-	        flag = true;  
-	    }  
-	    return flag;  
-	}  
+
+	/**
+	 * 删除单个文件
+	 * 
+	 * @param sPath
+	 *            被删除文件的文件名
+	 * @return 单个文件删除成功返回true，否则返回false
+	 */
+	public static boolean deleteFile(String sPath) {
+		boolean flag = false;
+		File file = new File(sPath);
+		// 路径为文件且不为空则进行删除
+		if (file.isFile() && file.exists()) {
+			file.delete();
+			flag = true;
+		}
+		return flag;
+	}
+
+	/**
+	 * 删除目录（文件夹）以及目录下的文件
+	 * 
+	 * @param sPath
+	 *            被删除目录的文件路径
+	 * @return 目录删除成功返回true，否则返回false
+	 */
+	public static boolean deleteDirectory(String sPath) {
+		// 如果sPath不以文件分隔符结尾，自动添加文件分隔符
+		if (!sPath.endsWith(File.separator)) {
+			sPath = sPath + File.separator;
+		}
+		File dirFile = new File(sPath);
+		// 如果dir对应的文件不存在，或者不是一个目录，则退出
+		if (!dirFile.exists() || !dirFile.isDirectory()) {
+			return false;
+		}
+		boolean flag = true;
+		// 删除文件夹下的所有文件(包括子目录)
+		File[] files = dirFile.listFiles();
+		for (int i = 0; i < files.length; i++) {
+			// 删除子文件
+			if (files[i].isFile()) {
+				flag = deleteFile(files[i].getAbsolutePath());
+				if (!flag)
+					break;
+			} // 删除子目录
+			else {
+				flag = deleteDirectory(files[i].getAbsolutePath());
+				if (!flag)
+					break;
+			}
+		}
+		if (!flag)
+			return false;
+		// 删除当前目录
+		if (dirFile.delete()) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public static void createDir(String path) {
+		File theDir = new File(path);
+		// if the directory does not exist, create it
+		if (!theDir.exists()) {
+			try {
+				theDir.mkdir();
+			} catch (SecurityException se) {
+				// handle it
+			}
+		}
+	}
 
 }
