df1c514fec2ee54f9dec6754f73780fdd21f8808
542113578@qq.com <542113578@qq.com>
2015-07-23 14:47:59 +0800

    update

---
+++ src/main/java/com/wuba/result/TestCaseLoop.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package com.wuba.result;
+
+import java.io.IOException;
+
+import org.kxml2.io.KXmlSerializer;
+
+import com.wuba.report.XMLParser;
+
+/**
+ * @author hui.qian qianhui@58.com  
+ * @date 2015年7月23日 下午2:38:11
+ */
+public class TestCaseLoop implements XMLParser{
+
+	/* (non-Javadoc)
+	 * @see com.wuba.report.XMLParser#serialize(org.kxml2.io.KXmlSerializer)
+	 */
+	@Override
+	public void serialize(KXmlSerializer serializer) throws IOException {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ src/main/java/com/wuba/result/TestRunner.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package com.wuba.result;
+
+/**
+ * @author hui.qian qianhui@58.com  
+ * @date 2015年7月23日 下午2:47:29
+ */
+public class TestRunner {
+
+}
