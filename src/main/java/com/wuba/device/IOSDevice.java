f71c6ba204d3044488d04f93d6a8d86693873118
vigossjjj <jiangyunpeng@58.com>
2015-07-27 16:36:03 +0800

    remove unused

--- src/main/java/com/wuba/device/IOSDevice.java
+++ src/main/java/com/wuba/device/IOSDevice.java
@@ -1,13 +1,9 @@
 package com.wuba.device;
 
-import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 import org.robovm.libimobiledevice.IDevice;
 import org.robovm.libimobiledevice.LockdowndClient;
