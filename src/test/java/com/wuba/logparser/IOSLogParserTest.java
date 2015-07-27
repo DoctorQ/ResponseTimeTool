8098e989b029c00fa78ecc3dbfd8582a4856d79c
542113578@qq.com <542113578@qq.com>
2015-07-27 15:17:09 +0800

    update

--- src/test/java/com/wuba/logparser/IOSLogParserTest.java
+++ src/test/java/com/wuba/logparser/IOSLogParserTest.java
@@ -3,21 +3,30 @@
  */
 package com.wuba.logparser;
 
+import java.io.File;
+
 import org.testng.annotations.BeforeGroups;
+import org.testng.annotations.Test;
 
 /**
- * @author hui.qian qianhui@58.com  
+ * @author hui.qian qianhui@58.com
  * @date 2015年7月27日 上午11:38:59
  */
 public class IOSLogParserTest {
-	
+
 	private LogParser logParser;
-	
+	private File file;
+
 	@BeforeGroups(groups = "unittest")
 	public void setUp() {
 		logParser = new IOSLogParser();
+		file = new File("");
 	}
-	
-	
+
+	@Test(groups = { "unittest" })
+	public void parserLogTest() {
+		logParser.parserLog(file);
+
+	}
 
 }
