02df6bdd3887bccab40417bb11e4691b4b16b8e0
jiayue <jiayue@58.com>
2015-07-28 11:48:10 +0800

    UI

--- src/main/java/com/wuba/gui/SelectDeviceDialog.java
+++ src/main/java/com/wuba/gui/SelectDeviceDialog.java
@@ -1,24 +1,35 @@
 package com.wuba.gui;
 
 import java.awt.BorderLayout;
+import java.awt.Container;
 import java.awt.FlowLayout;
+import java.awt.GridLayout;
 
 import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+
 import java.awt.Font;
 import java.awt.Color;
+
 import javax.swing.JScrollPane;
 import javax.swing.JList;
 import javax.swing.AbstractListModel;
+
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 
+import javax.swing.JTextArea;
+import javax.swing.border.BevelBorder;
+import javax.swing.border.MatteBorder;
+import javax.swing.ListSelectionModel;
+
+@SuppressWarnings({ "unused", "serial" })
 public class SelectDeviceDialog extends JDialog {
 
-	private final JPanel SelectDeviceDialog = new JPanel();
-
+	private final JScrollPane selectDeviceListPane = null;
+	private Container container = null;
 	/**
 	 * Launch the application.
 	 */
@@ -36,24 +47,25 @@ public class SelectDeviceDialog extends JDialog {
 	 * Create the dialog.
 	 */
 	public SelectDeviceDialog() {
+		//whole Jdialog
 		setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
-		setForeground(Color.PINK);
 		setFont(new Font("Lucida Grande", Font.PLAIN, 14));
 		setTitle("Select a Device");
-		setBounds(100, 100, 230, 162);
-		getContentPane().setLayout(new BorderLayout());
-		SelectDeviceDialog.setLayout(new FlowLayout());
-		SelectDeviceDialog.setBorder(new EmptyBorder(5, 5, 5, 5));
-		getContentPane().add(SelectDeviceDialog, BorderLayout.CENTER);
+		setBounds(100, 100, 231, 183);
+		
 		{
-			JScrollPane selectDeviceListPane = new JScrollPane();
-			selectDeviceListPane.setToolTipText("select device dialog");
-			SelectDeviceDialog.add(selectDeviceListPane);
+			JScrollPane deviceListScrollPane = new JScrollPane();
+			getContentPane().add(deviceListScrollPane,BorderLayout.CENTER);
 			{
+				@SuppressWarnings("rawtypes")
 				JList deviceList = new JList();
-				deviceList.setVisibleRowCount(5);
+				deviceList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+				deviceList.setValueIsAdjusting(true);
+				deviceList.setBorder(null);
+				deviceList.setFont(new Font("Lucida Grande", Font.PLAIN, 13));
+				deviceList.setForeground(Color.BLACK);
 				deviceList.setModel(new AbstractListModel() {
-					String[] values = new String[] {"1111111111111111111111111", "2222222222222222222222222", "3333333333333333333333333", "4444444444444444444444444"};
+					String[] values = new String[] {"11111111111111111111", "22222222222222222222", "33333333333333333333", "44444444444444444444", "55555555555555555555", "66666666666666666666", "77777777777777777777", "88888888888888888888", "99999999999999999999"};
 					public int getSize() {
 						return values.length;
 					}
@@ -61,13 +73,15 @@ public class SelectDeviceDialog extends JDialog {
 						return values[index];
 					}
 				});
-				selectDeviceListPane.setViewportView(deviceList);
+				deviceListScrollPane.setViewportView(deviceList);
 			}
 		}
+		//button panel
 		{
 			JPanel buttonPane = new JPanel();
 			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
 			getContentPane().add(buttonPane, BorderLayout.SOUTH);
+			//ok button
 			{
 				JButton okButton = new JButton("OK");
 				okButton.addActionListener(new ActionListener() {
@@ -79,7 +93,7 @@ public class SelectDeviceDialog extends JDialog {
 				buttonPane.add(okButton);
 				getRootPane().setDefaultButton(okButton);
 			}
-			
+			//cancel button
 			{
 				JButton cancelButton = new JButton("Cancel");
 				cancelButton.addActionListener(new ActionListener() {
