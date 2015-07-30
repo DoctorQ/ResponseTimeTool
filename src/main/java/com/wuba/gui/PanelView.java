92fb3fcdba9eacbe3f67e49059df9f5e294bbe09
542113578@qq.com <542113578@qq.com>
2015-07-30 17:15:37 +0800

    update

--- src/main/java/com/wuba/gui/PanelView.java
+++ src/main/java/com/wuba/gui/PanelView.java
@@ -1,17 +1,11 @@
 package com.wuba.gui;
 
-import java.awt.Color;
-import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.image.BufferedImage;
-import java.io.File;
 import java.io.IOException;
 
-import javax.imageio.ImageIO;
-import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
-import javax.swing.SwingUtilities;
 
 import com.wuba.device.AndroidDevice;
 import com.wuba.device.DeviceManager;
